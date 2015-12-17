5db99f283566d32e4091e9c8a3dc4b5bafc9748e
Kelly Lampotang <lampotan@usc.edu>
2015-12-17 02:02:50 -0500

    initial commit

---
+++ Podfile.swift
@@ -0,0 +1,15 @@
+//
+//  Podfile.swift
+//  TheOtherSide
+//
+//  Created by Kelly Lampotang on 12/16/15.
+//  Copyright © 2015 KW. All rights reserved.
+//
+
+import Foundation
+
+source 'https:://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+pod 'GoogleMaps'
+
+xcodeproj 'MyProject'
