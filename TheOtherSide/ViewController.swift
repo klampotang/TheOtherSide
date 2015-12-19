530033f295cc1a477354f27cd420f8f596fff41e
Kelly Lampotang <lampotan@usc.edu>
2015-12-19 14:18:03 -0500

    fetches, prints, and displays current location details

--- TheOtherSide/ViewController.swift
+++ TheOtherSide/ViewController.swift
@@ -10,13 +10,82 @@ import UIKit
 import GoogleMaps
 
 
-class ViewController: UIViewController, GMSMapViewDelegate {
+class ViewController: UIViewController, GMSMapViewDelegate,CLLocationManagerDelegate
+
+ {
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var WhereAmI: UIButton!
     
-    override func loadView() {
+    /*override func loadView() {
         let panoView = GMSPanoramaView(frame: CGRectZero)
         panoView.delegate = self
         self.view = panoView
         panoView.moveNearCoordinate(CLLocationCoordinate2DMake(0.000, 150.312))
+        
+    }*/
+    
+    @IBAction func findMyLocation(sender: UIButton) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    /*class ViewController: UIViewController {
+    */
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    let locationManager = CLLocationManager()
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                let pm = placemarks![0] 
+                self.displayLocationInfo(pm)
+                
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark?) {
+        if let containsPlacemark = placemark {
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode : ""
+            let administrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+            print(containsPlacemark.location!.coordinate.latitude)
+            print(containsPlacemark.location!.coordinate.longitude)
+            print(locality)
+            print(postalCode)
+            print(administrativeArea)
+            print(country)
+            self.city.text = locality
+            self.state.text = administrativeArea
+            self.country.text = country
+            
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,23 +93,15 @@ class ViewController: UIViewController, GMSMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    /*class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    }
-    */
-    
-    
-    
 }
 
 extension ViewController: GMSPanoramaViewDelegate {
     func panoramaView(view: GMSPanoramaView!, error: NSError!, onMoveNearCoordinate coordinate: CLLocationCoordinate2D) {
         print("\(coordinate.latitude) \(coordinate.longitude) not available")
-        performSegueWithIdentifier("ErrorSegue", sender: nil)
+        //performSegueWithIdentifier("ErrorSegue", sender: nil)
         
     }
     
 }
+
+
